89596a842d301fc1c87e3e8c05ba303e4bb55da7
cinnamuri2021 <85371687+cinnamuri2021@users.noreply.github.com>
2024-09-19 00:00:17 -0700

    add the files for code deploy

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull cinnamuridocker9/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 cinnamuridocker9/simple-python-flask-app
 
