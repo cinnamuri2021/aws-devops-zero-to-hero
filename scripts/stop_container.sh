0303eaa1e175e819edd4da459c2fb85ea89bfc8a
cinnamuri2021 <85371687+cinnamuri2021@users.noreply.github.com>
2024-09-19 00:39:33 -0700

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,5 +2,5 @@
 set -e
 
 # Stop the running container (if any)
-containerid=$(sudo docker ps --format "{{.ID}}" | head -n 1)
+containerid=$(docker ps --format "{{.ID}}" | head -n 1)
 docker rm -f $containerid
